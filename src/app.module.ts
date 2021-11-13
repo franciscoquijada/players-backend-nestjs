03d33b522031676e9b17fe6e37337544ceda8da8
fquijada <fquijada@centribal.com>
2021-11-13 13:41:13 -0300

    feat: create module players

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PlayersModule } from './modules/players/players.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PlayersModule,
+  ],
 })
 export class AppModule {}
