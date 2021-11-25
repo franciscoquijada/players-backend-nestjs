cdd20adfd60f8cc0274918d5680db5be537cfc41
fquijada <fquijada@centribal.com>
2021-11-25 20:00:25 -0300

    format: add validation isInt in dto page and limit

--- src/commons/pagination/findWithPagination.dto.ts
+++ src/commons/pagination/findWithPagination.dto.ts
@@ -1,20 +1,23 @@
-import { IsNumber, Min, IsOptional, Matches } from 'class-validator';
+import {IsNumber, Min, IsOptional, Matches, IsString, IsInt} from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class FindWithPaginationDto {
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @IsNumber()
   @Min(1)
   page?: number = 1;
 
   @IsOptional()
   @Type(() => Number)
+  @IsInt()
   @IsNumber()
   @Min(1)
   limit?: number = 20;
 
   @IsOptional()
+  @IsString()
   @Matches(/^[a-z0-9\u00f1\u00d1 _]+$/i, {
     message: 'search $value must be numbers or letters',
   })
