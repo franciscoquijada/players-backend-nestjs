153b2dc68acc49dae0c221d99ee13c5e48682a90
fquijada <fquijada@centribal.com>
2021-11-25 19:30:52 -0300

    feature: add validation to dto PaginationDto, add forbiddenNonWhitelidted and transformOptions to global ValidationPipe

--- src/commons/pagination/pagination.dto.ts
+++ src/commons/pagination/pagination.dto.ts
@@ -1,6 +1,20 @@
+import {Type} from "class-transformer";
+import {IsNumber, Min, IsOptional, IsArray} from 'class-validator';
+
 export class PaginationDto {
+  @IsArray()
+  @Type(() => Array)
   data: [];
+
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
   total: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
   page: number;
 
   constructor(data, total, page) {
